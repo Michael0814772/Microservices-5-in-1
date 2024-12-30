de1ecd2ebc16cab0107e10d4ea71c7152e1700f2
Michael Akinrinmade <michael.akinrinmade@stanbicibtc.com>
2024-12-30 12:32:02 +0100

    start of spring cloud

---
+++ limits-service/src/main/java/com/microservices_5in1/limits_service/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices_5in1.limits_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
---
+++ limits-service/src/test/java/com/microservices_5in1/limits_service/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_5in1.limits_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
